d9b154ad0d6f964914ca8948a1606c72f295d28f
Benjamin Boeckman <bboeckman3@gatech.edu>
2021-09-26 02:20:51 -0400

    Adding file browse utility code

---
+++ Utilities/Popups/fileBrowser.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+
+# create the root window
+root = tk.Tk()
+root.title('Open File Dialog')
+root.resizable(False, False)
+root.geometry('300x150')
+
+
+def select_file():
+    filetypes = (
+        ('csv files', '*.csv'),
+        ('All files', '*.*')
+    )
+
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
+    showinfo(
+        title='Selected File',
+        message=filename
+    )
+
+
+# open button
+open_button = ttk.Button(
+    root,
+    text='Open a File',
+    command=select_file
+)
+
+open_button.pack(expand=True)
+
+
+# run the application
+root.mainloop()
