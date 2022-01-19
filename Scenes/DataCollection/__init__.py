a819be312bf6c0a2d1fb8d0c7f7857927c03b337
Faris Durrani <afarisdurrani@gmail.com>
2022-01-18 23:14:02 -0500

    Minor: removed comment

--- Scenes/DataCollection/__init__.py
+++ Scenes/DataCollection/__init__.py
@@ -142,11 +142,6 @@ class DataCollection(DAATAScene, uiFile):
             self.is_data_collecting.clear()
             self.popup_dataSaveLocation("DataCollection",
                                         self.collection_start_time)
-            # conf = self.popup_stopDataConfirmation()
-            # if conf == QtWidgets.QDialog.Accepted:
-            #     self.button_display.setText("Start Collecting Data")
-            #     self.is_data_collecting.clear()
-            #     self.popup_dataSaveLocation()
 
     def slot_checkbox_state_change(self):
         if self.selectAll_checkbox.isChecked():
