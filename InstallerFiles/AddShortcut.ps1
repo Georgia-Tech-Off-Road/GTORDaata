d1c20b5b1a7820b07be25c5ceaff1c34d942452f
Benjamin <bboeckman3@gatech.edu>
2022-02-13 16:23:25 -0500

    Fixing wrong shortcut icon path

--- InstallerFiles/AddShortcut.ps1
+++ InstallerFiles/AddShortcut.ps1
@@ -3,7 +3,7 @@ $batchPath="C:\Program Files (x86)\GTOR DAATA\InstallerFiles\RunDAATA.bat"
 $objShell = New-Object -ComObject WScript.Shell
 $objShortCut = $objShell.CreateShortcut("C:\Program Files (x86)\GTOR DAATA\GTOR DAATA.lnk")
 $objShortCut.TargetPath = $batchPath
-$objShortCut.IconLocation = "C:\Program Files\GTOR DAATA\InstallerFiles\icon_GTORLogo.ico"
+$objShortCut.IconLocation = "C:\Program Files (x86)\GTOR DAATA\InstallerFiles\icon_GTORLogo.ico"
 $objShortCut.Save()
 
 $batchName = 'Run DAATA'
@@ -11,5 +11,5 @@ $batchPath="C:\Program Files (x86)\GTOR DAATA\InstallerFiles\RunDAATA.bat"
 $objShell = New-Object -ComObject WScript.Shell
 $objShortCut = $objShell.CreateShortcut("$Home\Desktop\GTOR DAATA.lnk")
 $objShortCut.TargetPath = $batchPath
-$objShortCut.IconLocation = "C:\Program Files\GTOR DAATA\InstallerFiles\icon_GTORLogo.ico"
+$objShortCut.IconLocation = "C:\Program Files (x86)\GTOR DAATA\InstallerFiles\icon_GTORLogo.ico"
 $objShortCut.Save()
