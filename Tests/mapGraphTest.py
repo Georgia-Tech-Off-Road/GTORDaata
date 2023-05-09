a2beabbd05a2b1636078b30922eb13c55b347dc5
Param Pithadia <ppithadia6@gatech.edu>
2023-05-09 14:08:21 -0400

    Constant updating to gps graphing
    Still need to see how to get the data from daata to this script.

--- Tests/mapGraphTest.py
+++ Tests/mapGraphTest.py
@@ -1,18 +1,43 @@
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.animation
+import numpy as np
 
+# Set coordinate bounds of map
 boundsBox = [44.09426, 44.08128, -88.53080, -88.51538]
-oshkosh_m = plt.imread('c:/Users/samue/Downloads/map.png')
+# Make a test middle point on the map
+latPoint = prevLatPoint = (boundsBox[0] + boundsBox[1]) / 2
+longPoint = prevLongPoint = (boundsBox[2] + boundsBox[3]) / 2
+# Read downloaded map image using boundsBox coordinates
+oshkosh_m = plt.imread('map.png')
 
 fig, ax = plt.subplots(figsize = (8,7))
+ax.imshow(oshkosh_m, zorder=0, extent = boundsBox, aspect= 'equal')
 ax.set_title('Plotting Spatial Data on Oshkosh Map')
 ax.set_xlim(boundsBox[0],boundsBox[1])
 ax.set_ylim(boundsBox[2],boundsBox[3])
-ax.imshow(oshkosh_m, zorder=0, extent = boundsBox, aspect= 'equal')
+x, y = [],[]
+x.append(prevLatPoint)
+y.append(prevLongPoint)
+sc = ax.scatter(x,y)
 
-latPoint = 44.08626
-longPoint = -88.52080
-ax.scatter(latPoint, longPoint, zorder=1, alpha= 1, c='r', s=10)
+def animate(i):
+    global latPoint
+    global prevLatPoint
+    global longPoint
+    global prevLongPoint
 
+    latPoint += 0.0001
+    longPoint += 0.0001
+
+    if ((latPoint != prevLatPoint) or (longPoint != prevLongPoint)):
+        x.clear()
+        y.clear()
+        x.append(latPoint)
+        y.append(longPoint)
+        sc.set_offsets(np.c_[x,y])
+        prevLatPoint = latPoint
+        prevLongPoint = longPoint
+#        plt.pause(0.5)
+
+ani = matplotlib.animation.FuncAnimation(fig, animate, frames=2, interval=100, repeat=True) 
 plt.show()
